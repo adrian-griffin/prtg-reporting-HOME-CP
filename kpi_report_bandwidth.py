fdf5e39b60fa1eca199d6019da5c6b0e81597f82
adrian-griffin <71678657+adrian-griffin@users.noreply.github.com>
2022-09-17 19:09:57 -0600

    1

--- kpi_report_bandwidth.py
+++ kpi_report_bandwidth.py
@@ -6,7 +6,8 @@
 #   
 #       Average runtime: 274 seconds (4.5 - 5.0 minutes)
 # -------------------------------------------------------------------------------
-from time import time
+from time import time, time_ns
+from turtle import width
 import requests
 import re
 import math
@@ -17,21 +18,10 @@ import json
 import datetime
 import argparse
 import openpyxl as opyxl
-import pandas as pd
 import os
-import subprocess
-import logging
 
 ###########################################################
 ### [Most primary functions declared here]
-
-#############
-### [Efficiency Checking Imports]
-######################
-import cProfile
-import pstats
-import timeit   
-
 
 ### [Getting script's current working directory to be used later to ensure that 
 #       no exceptions arise and for safer file IO]
@@ -134,40 +124,46 @@ current_sys_datetime = datetime.datetime.now()
 '''-------------------------------------------- MAIN --------------------------------------------'''
 '''----------------------------------------------------------------------------------------------'''
 ####################################################################################################
+global outputMainSheet
 
-global primary_df
-global secondary_df
-global summary_df
-primary_df = pd.DataFrame(
-   {'Location':["","","","","",],
-    'MaxTraffic':["","","","","",],
-    'ChokePoint':["","","","","",],
-    'ChokePointLimit':["","","","","",],        
-    'CircuitMaxLimit':["","","","","",],        
-    'CircuitUtilization':["","","","","",],        
-    'ChokePointUtilization0':["","","","","",],        
-    'ChokePointUtilization1':["","","","","",],        
-    'ChokePointUtilization2':["","","","","",],        
-    'ChokePointUtilization3':["","","","","",],          
-    'MaxPlanUsage':["","","","","",],            
-    'Notes':["","","","","",],    
-    'Action':["","","","","",]})
+outputWorkbook = opyxl.Workbook()
+outputMainSheet = outputWorkbook.active
 
-secondary_df = pd.DataFrame(
-   {'kpi_choke':["","","","","",],
-    'kpi_chokelimit':["","","","","",],
-    'kpi_cktmaxlimit':["","","","","",],
-    'ChokePointLimit':["","","","","",]})
+t0BACK_headers_u,t14BACK_headers_u = timeWindowFrames("0")
+t7BACK_headers_u,t21BACK_headers_u = timeWindowFrames("1")
+t14BACK_headers_u,t28BACK_headers_u = timeWindowFrames("2")
+t21BACK_headers_u,t35BACK_headers_u = timeWindowFrames("3")
 
-summary_df = pd.DataFrame(
-   {'Core':["","","","","",],
-    'Bandwidth':["","","","","",],
-    'MaxCapacity':["","","","","",],
-    'Utilization0':["","","","","",],
-    'Utilization1':["","","","","",],
-    'Utilization2':["","","","","",]})
+t0BACK_headers = t0BACK_headers_u.strftime('%m/%d')
+t7BACK_headers = t7BACK_headers_u.strftime('%m/%d')
+t14BACK_headers = t14BACK_headers_u.strftime('%m/%d')
+t21BACK_headers = t21BACK_headers_u.strftime('%m/%d')
+t28BACK_headers = t28BACK_headers_u.strftime('%m/%d')
+t35BACK_headers = t35BACK_headers_u.strftime('%m/%d')
 
 
+sheetHeaders = ['Location','Max Traffic (Mb/s)','Choke Point','Choke Point Limit (Mb/s)','Circuit Max Limit (Mb/s)','Circuit Utilization (%)',
+    f'Choke Utilization ({t0BACK_headers} - {t14BACK_headers}) (%)',f'Choke Utilization ({t7BACK_headers} - {t21BACK_headers}) (%)',f'Choke Utilization ({t14BACK_headers} - {t28BACK_headers}) (%)',f'Choke Utilization ({t21BACK_headers} - {t35BACK_headers}) (%)',
+    'Max Usage Plan','Notes','Action']
+
+coreUtilSummaryHeaders = ['Core Utilization Summary','Bandwidth (Mb/s)',
+    'Gross Utilization (Current) (%)','Gross Utilization (7 Days Ago - 14 Days Ago) (%)','Gross Utilization (14 - 21 Days Ago) (%)']
+
+alphabetArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M']
+
+for letter in alphabetArray:
+    outputMainSheet.column_dimensions[str(letter)].width = '25'
+outputMainSheet.column_dimensions['A'].width = '25'
+
+for i in range(0,len(sheetHeaders)):
+    outputMainSheet[str(alphabetArray[i])+'7']=sheetHeaders[i]
+    outputMainSheet[str(alphabetArray[i])+'7'].alignment = opyxl.styles.Alignment(horizontal='general', vertical='bottom', text_rotation=0, wrap_text=True, shrink_to_fit=False, indent=0)
+
+
+for i in range(0,len(coreUtilSummaryHeaders)):
+    outputMainSheet[str(alphabetArray[i])+'1']=coreUtilSummaryHeaders[i]
+    outputMainSheet[str(alphabetArray[i])+'1'].alignment = opyxl.styles.Alignment(horizontal='general', vertical='bottom', text_rotation=0, wrap_text=True, shrink_to_fit=False, indent=0)
+outputMainSheet['A5']='Total: '
 
 ### [Query/Call PRTG API]
 #############
@@ -253,44 +249,10 @@ def extract_tags(sensor):
 
 ### [Beginning initial PRTG API call and assigning data to "sensors" var for manipulation]
 #############
-sensorsMainCall = get_kpi_sensor_ids(cliargs.username, PRTG_PASSWORD)
+sensorsMainCall = get_kpi_sensor_ids(cliargs.username, PRTG_PASSWORD)     
 
-### [CREATION OF COMPLETE FILE (CURRENLT JUST THE SUMMARY)]
-#############
-def summary_out():
-    segments = set()
-    for data in summary_data:
-        # Create set from all the segment values (creates a unique list)
-        segments.add(data.get('segment'))
-
-    segment_bandwidth_total = (1*10^(-25))
-    segment_capacity_total = (1*10^(-25))
-
-    i = 0
-    for segment in segments:
-        segment_bandwidth = (1*10^(-25))
-        segment_limit = (1*10^(-25))
-        for data in summary_data:
-            if data['segment'] == segment:
-                segment_bandwidth += int(data['bandwidth'])
-                segment_limit += int(data['limit'])
-        saturation = segment_bandwidth / segment_limit
-        segment_bandwidth_total += segment_bandwidth
-        segment_capacity_total += segment_limit
-        segment_saturation = segment_bandwidth_total / segment_capacity_total
-
-        summary_df['Core'][i].append(segment)
-        summary_df['Bandwidth'][i].append(segment_bandwidth)
-        summary_df['MaxCapacity'][i].append(segment_limit)
-        summary_df['Utilization'][i].append(saturation)
-
-        summary_df['Core'][i].append('Total')
-        summary_df['Bandwidth'][i].append(segment_bandwidth_total)
-        summary_df['MaxCapacity'][i].append(segment_bandwidth_total)
-        summary_df['Utilization'][i].append(segment_saturation)        
-
-def extraChokeUtilCalc(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,summary_data,sensorsMainCall,sensor,i_index):
-    for k in range(1,3):
+def extraChokeUtilCalc(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,sensorsMainCall,sensor,i_index):
+    for k in range(1,4):
         Tstart,Tend = timeWindowFrames(k)
         response = requests.get(
                     f'https://{PRTG_HOSTNAME}/api/historicdata.json?id={sensor["objid"]}'
@@ -305,44 +267,33 @@ def extraChokeUtilCalc(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,summary_data,sensorsM
             traffic_out = normalize_traffic(data, 'Traffic Out (speed)')
             device_name = sensor['device'].split(' (')[0]
 
-
-
-            ### [dec] - [CHOKE POINT LIMIT (Mb/s)]
-            #############
-            if properties.get('kpi_chokelimit'):
-                secondary_df['kpi_chokelimit'][i_index].append(properties['kpi_chokelimit'])
-            else:
-                secondary_df['kpi_chokelimit'][i_index].append('NA')
-
-
             ### [dec] - [MAX TRAFFIC (Mb/s)]
             #############
             max_traffic = 0
             if properties.get('kpi_trafficdirection') == 'up':
                 if traffic_out == []:
-                    primary_df['MaxTraffic'][i_index].append("NA")
+                    max_traffic_san = 0
                 else:
                     max_traffic = math.ceil(numpy.percentile(traffic_out, int(cliargs.percentile)))
-                    primary_df['MaxTraffic'][i_index].append(max_traffic)
+                    max_traffic_san = max_traffic
             else:
                 if traffic_in == []:
-                    primary_df['MaxTraffic'][i_index].append("NA")
+                    max_traffic_san = 0
                 else:
                     max_traffic = math.ceil(numpy.percentile(traffic_in, int(cliargs.percentile)))
-                    primary_df['MaxTraffic'][i_index].append(max_traffic)
-
-            if "Core" in properties.get('kpi_seg'):
-                summary_data.append({'segment': properties.get('kpi_seg'),
-                                    'bandwidth': max_traffic,
-                                    'limit': properties.get('kpi_cktmaxlimit')})
-        
+                    max_traffic_san = max_traffic
 
             ### [dec] - [CHOKE POINT UTILIZATION (%)]
             #############
+            from openpyxl.formatting.rule import ColorScaleRule
+            rule = ColorScaleRule(start_type='percentile', start_value=0, start_color='FFAA0000',
+                end_type='percentile', end_value=100, end_color='FF00AA00')
+
             if properties.get('kpi_chokelimit'):
-                secondary_df['kpi_chokelimit'][i_index].append(max_traffic / int(properties['kpi_chokelimit']))
+                outputMainSheet.cell(row=int(i_index),column=7+int(k)).value=(float(max_traffic) / float(properties['kpi_chokelimit']))
+                outputMainSheet.cell(row=int(i_index),column=7+int(k)).style='Percent'
             else:
-                secondary_df['kpi_chokelimit'][i_index].append('NA')
+                outputMainSheet.cell(row=int(i_index),column=7+int(k)).value='NA'
             
              
         else:
@@ -350,9 +301,17 @@ def extraChokeUtilCalc(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,summary_data,sensorsM
             print("HTTP response 200: OK was not received")
             print("Received response code: "+str(response.status_code))
             exit(1)
+    k += 1
+    outputWorkbook.save("test.xlsx")
 
-def sensorsFrameCall(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,summary_data,sensorsMainCall):
-    i = 5
+def sensorsFrameCall(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,sensorsMainCall):
+    i = 8
+    s_count = 1
+    kpi_seg_arr = []
+
+    def outputToSheet(yIndex,xIndex,vIndex):
+        outputMainSheet.cell(row=int(yIndex),column=int(xIndex)).value=vIndex
+
     for sensor in sensorsMainCall:
         response = requests.get(
                 f'https://{PRTG_HOSTNAME}/api/historicdata.json?id={sensor["objid"]}'
@@ -372,78 +331,87 @@ def sensorsFrameCall(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,summary_data,sensorsMai
             ### [dec] - [LOCATION (Location)]
             #############
             if properties.get('kpi_siteid'):
-                loc_arr[i].append(re.sub("#", " ", properties['kpi_siteid']))
+                outputToSheet(i,1,re.sub("#", " ", properties['kpi_siteid']))
             else:
-                loc_arr[i].append('NA')
+                outputToSheet(i,1,'NA')
 
             if cliargs.debug:
                 # Device name (debug)
-                loc_arr[i].append(device_name)
-
+                outputToSheet(i,1,device_name)
+                
                 # Device id (debug)
-                loc_arr[i].append(sensor['objid'])
+                #outputToSheet(i,1,sensor['objid'])
 
             ### [dec] - [MAX TRAFFIC (Mb/s)]
             #############
-            maxtraffic_arra = primary_df['MaxTraffic']
             max_traffic = 0
             if properties.get('kpi_trafficdirection') == 'up':
                 if traffic_out == []:
-                    maxtraffic_arra[i].append("NA")
+                    outputToSheet(i,2,'NA')
                 else:
                     max_traffic = math.ceil(numpy.percentile(traffic_out, int(cliargs.percentile)))
-                    maxtraffic_arra[i][i].append(max_traffic)
+                    outputToSheet(i,2,max_traffic)
             else:
                 if traffic_in == []:
-                    maxtraffic_arra[i][i].append("NA")
+                    outputToSheet(i,2,'NA')
                 else:
                     max_traffic = math.ceil(numpy.percentile(traffic_in, int(cliargs.percentile)))
-                    maxtraffic_arra[i][i].append(max_traffic)
+                    outputToSheet(i,2,max_traffic)
 
             if "Core" in properties.get('kpi_seg'):
-                summary_data.append({'segment': properties.get('kpi_seg'),
-                                    'bandwidth': max_traffic,
-                                    'limit': properties.get('kpi_cktmaxlimit')})
+                if properties.get('kpi_seg') in kpi_seg_arr:
+                    pass
+                else:
+                    outputToSheet(1+s_count,1,properties.get('kpi_seg'))
+                    outputToSheet(1+s_count,2,max_traffic)
+                    outputToSheet(1+s_count,3,(float(max_traffic)/float(properties.get('kpi_cktmaxlimit'))))
+                    outputMainSheet.cell(row=int(1+s_count),column=3).style='Percent'
+                    s_count += 1
+                    kpi_seg_arr.append(properties.get('kpi_seg'))
 
             ### [dec] - [CHOKE POINT (Device)]
             #############
             if properties.get('kpi_choke'):
-                secondary_df['kpi_choke'][i].append(properties['kpi_choke'])
+                outputToSheet(i,3,properties['kpi_choke'])
             else:
-                secondary_df['kpi_choke'][i].append('NA')
+                outputMainSheet.cell(row=int(i),column=3).value='NA'
+                outputToSheet(i,3,'NA')
 
             ### [dec] - [CHOKE POINT LIMIT (Mb/s)]
             #############
             if properties.get('kpi_chokelimit'):
-                secondary_df['kpi_chokelimit'][i].append(properties['kpi_chokelimit'])
+                outputToSheet(i,4,float(properties['kpi_chokelimit']))
+                outputMainSheet.cell(row=int(i),column=int(6)).style='Percent'
             else:
-                secondary_df['kpi_chokelimit'][i].append('NA')
+                outputToSheet(i,4,'NA')
             
 
 
             ### [dec] - [CIRCUIT MAX LIMIT (Mb/s)]
             #############
             if properties.get('kpi_cktmaxlimit'):
-                secondary_df['kpi_cktmaxlimit'][i].append(properties['kpi_cktmaxlimit'])
+                outputToSheet(i,5,float(properties['kpi_cktmaxlimit']))
             else:
-                secondary_df['kpi_cktmaxlimit'][i].append('NA')
+                outputToSheet(i,5,'NA')
 
             ### [dec] - [CIRCUIT UTILIZATION (%)]
             #############
             if properties.get('kpi_cktmaxlimit'):
-                secondary_df['kpi_cktmaxlimit'][i].append(max_traffic / int(properties['kpi_cktmaxlimit']))
+                outputToSheet(i,6,(float(max_traffic) / float(properties['kpi_cktmaxlimit'])))
+                outputMainSheet.cell(row=int(i),column=int(6)).style='Percent'
             else:
-                secondary_df['kpi_cktmaxlimit'][i].append('NA')
+                outputToSheet(i,6,'NA')
 
 
             ### [dec] - [CHOKE POINT UTILIZATION (%)]
             #############
             if properties.get('kpi_chokelimit'):
-                secondary_df['kpi_chokelimit'][i].append(max_traffic / int(properties['kpi_chokelimit']))
+                outputToSheet(i,7,(float(max_traffic) / float(properties['kpi_chokelimit'])))
+                outputMainSheet.cell(row=int(i),column=int(7)).style='Percent'
             else:
-                secondary_df['kpi_chokelimit'][i].append('NA')         
+                outputToSheet(i,7,'NA') 
 
-            out_array_w_extras = extraChokeUtilCalc(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,summary_data,sensorsMainCall,sensor,i)
+            extraChokeUtilCalc(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,sensorsMainCall,sensor,i)
 
             i += 1
         else:
@@ -452,19 +420,9 @@ def sensorsFrameCall(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,summary_data,sensorsMai
             print("Received response code: "+str(response.status_code))
             exit(1)
 
-
-
-### [Array to be used for piping TMP file lines into final file]
-#############
-summary_data = []
-summary_data.clear() # Flushing array values just in case -- I dont feel like exception handling and this is easier
-
-
 ### [PRTG API call and assigning data to "sensors" var]
 #############
-sensorsFrameCall(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,summary_data,sensorsMainCall)
-
-### [Calling summary_out to analyze data from TMP file and create Summary table in COMP file]
+sensorsFrameCall(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,sensorsMainCall)
 
 
 
@@ -474,91 +432,3 @@ sensorsFrameCall(PRTG_HOSTNAME,cliargs,PRTG_PASSWORD,summary_data,sensorsMainCal
 #   create two documents, one stores the normal device data temporarily. Once the workload is done the table is generated as normal, but is placed into a new document
 #   so it is at the top. All TMP data is then filed in below the table.]
 #############
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##########################
-###################################  [!!] The block of docstring-commented code below was primarily just for testing
-################################### though it can still be used to run performance tests on this program/script
-##########################          otherwise it can be deleted as well. I left it commented out just in case.
-'''###################
-
-
-# Nothing below this line is very important 
-# Mostly testing tools   
-----------------------------
-
-############
-### [cProfile profiling. Outputs to stdout by default. To save output to a file ...
-###     ... run python3 kpi_report_bandwidth.py > performanceOutput.txt]
-def programPerformanceProfile():
-    profiler = cProfile.Profile()
-    profiler.enable()
-    main()
-    profiler.disable()
-    stats = pstats.Stats(profiler).sort_stats('cumtime')
-    stats.print_stats() 
-
-if __name__ == "__main__":
-    
-### Though this script doesn't need to be fast or hyper-efficient, it does take ~5-7 minutes on average to finish,
-# and I gotfrom several performance tests of the script
-
-    import cProfile, pstats
-    import time
-
-    ############
-    ### [Testing total runtime (to check efficiency and look for improvements)]
-    ###     [Can be safely ignored or removed]
-    def runtimeTimer():
-        effTimeBegin = time.time()
-        main()
-        print("--- %s seconds ---" % (time.time() - effTimeBegin))
-        return effTimeBegin
-
-    ############
-    ### [Only one performance test can be run at a time in order to not stress the hardware or ...
-    ###     ... flood PRTG with API calls.]
-
-    performanceTesterVAL = 2   ###  Can be 1 or 2
-                               ### 1 = cProfile
-                               ### 2 = runtimeTimer
-    if performanceTesterVAL == 1:
-        perfOut_cProfile = programPerformanceProfile()   ##   Running cProfile and saving results to 'perfOut_cProfile' var.
-    elif performanceTesterVAL == 2:
-        runtimeTimer()              ##   Running time-to-execute timer. 
-    else:
-        main()
-
-'''
